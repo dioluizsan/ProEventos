8b25ad81d02a4af7f940c6448527a624c2765296
diogo Luiz dos santos <dioluizsan@gmail.com>
2021-10-26 12:15:11 -0300

    Novos Componentes + rotas +titulos

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './shared/nav/nav.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { EventoService } from './services/evento.service';
 import { FormsModule } from '@angular/forms';
@@ -17,6 +17,10 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { TituloComponent } from './shared/titulo/titulo.component';
 
 
 @NgModule({
@@ -25,7 +29,11 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     EventosComponent,
     PalestrantesComponent,
     NavComponent,
-    DateTimeFormatPipePipe
+    DateTimeFormatPipePipe,
+    PerfilComponent,
+    DashboardComponent,
+    ContatosComponent,
+    TituloComponent
 
    ],
   imports: [
