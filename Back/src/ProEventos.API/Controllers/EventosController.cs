46abbcbf00aff0a334a3f156be42e8182d1c87d0
diogo Luiz dos santos <dioluizsan@gmail.com>
2021-11-03 17:17:39 -0300

    Introducao de EventoDto

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -31,7 +31,24 @@ namespace ProEventos.API.Controllers
             {
                 var eventos = await _eventoService.GetAllEventosAsync(true);
                 if(eventos==null) return NotFound("Nenhum evento encontrado!");
-                return Ok(eventos);
+
+                var eventosRetorno = new List<EventoDto>();
+
+                foreach (var evento in eventos)
+                {
+                    eventosRetorno.Add(new EventoDto(){
+
+                         Id = evento.Id,
+                         Local = evento.Local,
+                         DataEvento = evento.DataEvento.toString(),
+                         Tema = evento.Tema,
+                         QtdPessoas = evento.QtdPessoas,
+                         ImagemURL = evento.ImagemURL,
+                         Telefone = evento.Telefone,
+                         Email = evento.Email
+                    });
+                }
+                return Ok(eventosRetorno);
             }
             catch (System.Exception ex)
             {
